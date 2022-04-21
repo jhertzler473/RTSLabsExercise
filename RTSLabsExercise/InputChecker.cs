b194f1dace8dbf52da934e7856badcbe604f1767
Jacob Hertzler <jhertzler473@gmail.com>
2022-04-21 14:14:58 -0400

    Initial commit

---
+++ RTSLabsExercise/InputChecker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTSLabsExercise
+{
+    public static class InputChecker
+    {
+        public static Dictionary<string, int> AboveBelow(List<int> integerList, int comparison, bool inclusive = false)
+        {
+            return new Dictionary<string, int>
+            {
+                { "above", integerList.Count(val => inclusive ? val >= comparison : val > comparison) },
+                { "below", integerList.Count(val => inclusive ? val <= comparison : val < comparison) }
+            };
+        }
+
+
+        public static string StringRotation(string originalString, int rotationAmount)
+        {
+            // mod out the full rotations
+            int baseRotation = rotationAmount % originalString.Length;
+
+            // append the rotation
+            return $"{originalString.Substring(originalString.Length - baseRotation, baseRotation)}{originalString.Substring(0, originalString.Length - baseRotation)}";
+        }
+    }
+}
---
+++ TestProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using RTSLabsExercise;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Non-inclusive");
+            foreach(KeyValuePair<string, int> keyval in InputChecker.AboveBelow(new List<int>() { 1, 5, 2, 1, 10 }, 6))
+                Console.WriteLine(keyval.Key + ": " + keyval.Value);
+
+            // use inclusive check
+            Console.WriteLine("Inclusive");
+            foreach (KeyValuePair<string, int> keyval in InputChecker.AboveBelow(new List<int>() { 1, 5, 2, 1, 10 }, 5, true))
+                Console.WriteLine(keyval.Key + ": " + keyval.Value);
+
+
+            Console.WriteLine("String rotations");
+            // original rotation
+            Console.WriteLine(InputChecker.StringRotation("MyString", 2));
+            // rotation past total string length
+            Console.WriteLine(InputChecker.StringRotation("MyString", 10));
+        }
+    }
+}
